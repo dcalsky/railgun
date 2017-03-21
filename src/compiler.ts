dd375f19ad08458433e914c25e2421cd6404590a
dcalsky <dcalsky@163.com>
2017-03-21 15:01:04 +0800

    simplify bindElment function in Compiler

--- src/compiler.ts
+++ src/compiler.ts
@@ -2,20 +2,21 @@
  * Created by Dcalsky on 2017/3/19.
  */
 
-const NODE_TYPE = {
-    element: 1,
-    text: 3
+const enum NODE_TYPE {
+    ELEMENT = 1,
+    TEXT = 3
 }
 
 const DIRECTIVE = {
     prefix: 'n-',
+    for: 'for',
     model: 'model',
     eventPrefix: 'on'
 }
 
 const utils = {
-    isDirective(attr) {
-        return attr.indexOf(DIRECTIVE.prefix)
+    isDirective(attr): boolean {
+        return attr.indexOf(DIRECTIVE.prefix) !== -1
     },
     getDirective(attrName: string) {
         return attrName.substring(2)
@@ -40,50 +41,45 @@ const utils = {
 
 export default class Compiler {
     private watcher
+
     constructor(root: HTMLElement, watcher) {
         this.watcher = watcher
-        let nodes = Array.prototype.slice.call(root.childNodes, 0)
-        nodes.forEach((node) => {
-            this.handleChildNode(node)
-        })
+        this._handleChilds(root.childNodes)
     }
 
-    handleChildNode(nodes: HTMLElement) {
-        let node = nodes as HTMLElement,
-            nodeType = node.nodeType
+    _handleChilds(nodes: NodeList) {
+        let nodesArray = Array.prototype.slice.call(nodes, 0)
+        nodesArray.forEach((node) => {
+            this.handleNode(node)
+        })
+    }
+    handleNode(node: HTMLElement) {
+        let nodeType = node.nodeType
         switch (nodeType) {
-            case NODE_TYPE.element:
+            case NODE_TYPE.ELEMENT:
                 this.bindElement(node)
                 break
-            case NODE_TYPE.text:
+            case NODE_TYPE.TEXT:
                 this.renderText(node)
                 break
         }
+        this._handleChilds(node.childNodes)
     }
-
     renderText(node: HTMLElement) {
         let reg = /\{{2}(.*?)\}{2}/g,
             text = node.textContent,
             results = reg.test(text)
+
         if (!results) return
         node.textContent = text.replace(reg, (_, key) => {
             this.watcher.addValueListener(key, val => {
                 node.textContent = text.replace(reg, (m, n) => {
-                    if (n == key) {
-                        return val
-                    } else {
-                        return this.watcher.getVal(n)
-                    }
+                    return n == key ? val : this.watcher.getVal(n)
                 })
             })
             return this.watcher.getVal(key)
         })
     }
-
-    // After bind, remove it
-    // Do not change the value of input which is been typing
-    // Get the initial value
-    // Add type event listener
     bindElement(node: HTMLElement | HTMLInputElement) {
         let attrs = node.attributes,
             watcher = this.watcher
@@ -92,22 +88,36 @@ export default class Compiler {
                 value = attr.value,
                 directive = utils.getDirective(name)
 
-            if (utils.isDirective(name) == -1) return
+            if (utils.isDirective(name)) return
+
             if (directive === DIRECTIVE.model) {
-                utils.update_input((<HTMLInputElement>node), watcher.getVal(value))
-                watcher.addValueListener(value, val => {
-                    if (val === (<HTMLInputElement>node).value) return
-                    utils.update_input((<HTMLInputElement>node), val)
-                })
-                node.addEventListener('input', function (event) {
-                    watcher.pubVal(value, (<HTMLInputElement>this).value)
-                })
+                this._handleModel(<HTMLInputElement>node, value)
             } else if (utils.getEventPrefix(directive) === DIRECTIVE.eventPrefix) {
-                let eventName = utils.getEventName(name)
-                node.addEventListener(eventName, this.watcher.getMethod(value))
+                this._handleEvent(node, utils.getEventName(name), value)
+            } else if (directive === DIRECTIVE.for) {
+                this._handleFor(node)
             }
+            // Finished attribute bind, remove it
             node.removeAttribute(name)
         })
         this.renderText(node)
+    }
+    _handleModel(node: HTMLInputElement, key: string) {
+        let watcher = this.watcher
+        utils.update_input(node, watcher.getVal(key))
+        watcher.addValueListener(key, val => {
+            // Do not change the value of input which is been typing
+            if (val === node.value) return
+            utils.update_input(node, val)
+        })
+        node.addEventListener('input', function (event) {
+            watcher.pubVal(key, this.value)
+        })
+    }
+    _handleEvent(node: HTMLElement, eventName: string, key: string) {
+        node.addEventListener(eventName, this.watcher.getMethod(key))
+    }
+    _handleFor(node) {
+
     }
 }
